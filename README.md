082f12301fc46d6b9b64580c1e7ceff1e448ae9e
kbseah <kbseah@mpi-bremen.de>
2016-12-08 21:33:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# gbtlite
+## Visualize metagenomes in a web browser
+
+View coverage-GC plots of metagenome assemblies in your web browser with `gbtlite`. These plots are useful in quality control of metagenomic data, and in guiding the binning of individual genomes from metagenomes.
+
+`gbtlite` implements these visualizations using the [D3.js library](http://d3js.org). 
+
+See the live demo here: 
+
+If you find this useful, consider trying the full [`gbtools` package for R](https://github.com/kbseah/genome-bin-tools), which allows selection and manipulation of bins, in addition to visualization.
+
+## Instructions
+
+Prepare your data for plotting. You need to have a metagenome assembly and the raw reads used to make that assembly. Calculate the coverage statistics for the assembly by mapping the reads to the assembly with bbmap.sh.
+
+The input, like for `gbtools`, is the "covstats" output from bbmap.sh, which contains the coverage statistics for each contig in an assembly. You'll have to manually (sorry) remove the `#` character from the header line of the file before feeding it to `gbtlite`.
+
+Refer to the `gbtools` manual for [instructions and example commands](https://github.com/kbseah/genome-bin-tools/wiki/1.-Produce-and-annotate-metagenomic-assembly) (only step 1a is necessary for `gbtlite`).
+
+## Citations and further reading
+* GC-coverage plots - Kumar et al. 2013. [Blobology: exploring raw genome data for contaminants, symbionts and parasites using taxon-annotated GC-coverage plots](http://journal.frontiersin.org/article/10.3389/fgene.2013.00237/abstract). Frontiers in Genetics 4. doi: 10.3389/fgene.2013.00237
+* Genome binning with coverage data - Albersten et al. 2013. [Genome sequences of rare, uncultured bacteria obtained by differential coverage binning of multiple metagenomes](http://www.nature.com/nbt/journal/v31/n6/abs/nbt.2579.html). Nature Biotechnology 31: 533-538.
+* `gbtools` for R - Seah & Gruber-Vodicka 2015. [gbtools: Interactive visualization of metagenome bins in R](http://journal.frontiersin.org/article/10.3389/fmicb.2015.01451/full). Frontiers in Microbiology 6. doi: 10.3389/fmicb.2015.01451
+
+## Contact
+Brandon Seah - kbseah@mpi-bremen.de
+Please report bugs etc. with the GitHub issues tracker 
---
+++ gbtlite.js
@@ -0,0 +1,275 @@
+// gbtlite.js for plotting coverage-GC plots of metagenomes
+
+
+// Check for FileReader API support
+if (!window.FileReader) {
+	alert("This browser does not support FileReader API."); 
+}
+
+// Number formatting functions
+var f3s = d3.format (".3s"); // SI prefix and 3 significant figures
+var f0pc = d3.format (".0%"); // Rounded percentage
+var f3r = d3.format (".3r"); // Rounded decimal 3 significant figures
+
+
+// Load coverage stats file
+//   Adapted from 
+//   http://stackoverflow.com/questions/36079390/parse-uploaded-csv-file-using-d3-js
+var reader = new FileReader();
+var data;
+
+function loadFile() {
+	var file = document.querySelector('input[type=file]').files[0];
+	reader.addEventListener("load",parseFile,false);
+	if(file) {
+        reader.readAsText(file);
+    }
+}
+
+function parseFile() {
+    var doesColumnExist = false;
+    data = d3.tsvParse(reader.result, function(d) {
+    	doesColumnExist = d.hasOwnProperty("Ref_GC");
+        return d;
+    });
+    // Interpret relevant fields as numeric - important!
+    data.forEach(function (d) { d.Avg_fold = +d.Avg_fold; } );
+    data.forEach(function (d) { d.Length = +d.Length; } );
+    data.forEach(function (d) { d.Ref_GC = +d.Ref_GC; } );
+    // Check for correct input
+    console.log(doesColumnExist);
+    if (! doesColumnExist) {
+    	alert("Please check your input file.");
+    }
+}
+
+// Define main chart size
+var margin = { top:25,
+    right: 100,
+    left: 50,
+    bottom: 50};
+var height = 600 - margin.top - margin.bottom;
+var width = 850 - margin.left - margin.right;
+
+// Define main chart object
+var chart = d3.select(".chart")
+	.attr("height",height + margin.bottom + margin.top)
+	.attr("width",width + margin.left + margin.right)
+	.append("g") // Append g object to space margins
+	.attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+// Background color for main chart plot area
+chart.append("rect")
+	.attr("height",height)
+	.attr("width",width)
+	.attr("fill","#feffe7");
+
+// Define length plot chart size
+var lenplotMargin = { top:20, right:20, left:50, bottom:20};
+var lenplotHeight = 350 - lenplotMargin.top - lenplotMargin.bottom;
+var lenplotWidth = 400 - lenplotMargin.left - lenplotMargin.right;
+
+// Define length plot chart object
+var lenplot = d3.select(".lenplot")
+	.attr("height",lenplotHeight + lenplotMargin.top + lenplotMargin.bottom)
+	.attr("width", lenplotWidth + lenplotMargin.left + lenplotMargin.right)
+	.append("g") // Space margins
+	.attr("transform","translate(" + lenplotMargin.left + "," + lenplotMargin.top + ")");
+// Background color 
+lenplot.append("rect")
+	.attr("height",lenplotHeight)
+	.attr("width",lenplotWidth)
+	.attr("fill", "#feffe7");
+
+// Div element for tooltip animations
+var div = d3.select("body")
+	.append("div")
+	.attr("class","tooltip")
+	.style("opacity",0);
+
+function drawGraph() { // This function is called when "draw" button is pressed
+
+    // Linear scale for x-axis (GC%)
+    var x = d3.scaleLinear()
+    	.domain([d3.min(data, function(d) {return d.Ref_GC; }) * 0.9,
+    		d3.max(data, function(d) {return d.Ref_GC; }) * 1.1
+   		 	])
+    	.range([0,width]);
+             
+    // Condition to ignore extreme low y-axis values in scale
+    var yMin = d3.min(data, function(d) {return d.Avg_fold;});
+    if (yMin < 0.05) {
+        yMin = 0.05;
+    } else {
+        yMin = +yMin;
+    }
+    
+    // Log scale for y-axis (coverage)
+    var y = d3.scaleLog()
+    	.domain([yMin / 2,
+        	// Div by 2 to avoid points directly on margin
+    		d3.max(data, function(d) {return d.Avg_fold; }) * 1.5
+        	// Mult by 1.5 to avoid points directly on margin
+    		])
+    	.range([height,1]);
+        // Invert min and max because SVG coord starts from upper left corner
+    // Sqrt scale for plot points (length)
+    var rad = d3.scaleSqrt()
+        .domain([0,d3.max(data, function(d) {return d.Length;} )])
+        .range([0,width*0.01]); // Max plot diameter is 2% of plot width
+
+    // horizontal axis
+    var xAxis = d3.axisBottom(x)
+        .ticks(10, ".0%"); // 10 tickmarks, as percentages
+    
+    chart.append("g")
+        .attr("class","x axis")
+        .attr("transform","translate(0," + height + ")")
+        .call(xAxis);
+    
+    // horizontal axis label
+    chart.append("text")
+        .attr("x",width/2)
+        .attr("y",height + margin.bottom + margin.top - 40)
+        .style("text-anchor","middle")
+        .style("font-family","sans-serif")
+        .style("font-size","10pt")
+        .text("GC %");
+    
+    // vertical axis
+    var yAxis = d3.axisLeft(y)
+        .ticks(10,".1s"); // 10 tickmarks, 1 sig fig with SI prefix
+    
+    chart.append("g")
+        .attr("class","y axis")
+        .call(yAxis);
+    
+    // vertical axis label
+    chart.append ("text")
+        .attr("transform","rotate(-90)")
+        .attr ("x",0 - (height/2))
+        .attr("y", -25)
+        .style("text-anchor","middle")
+        .style("font-family","sans-serif")
+        .style("font-size","10pt")
+        .text("Coverage");
+    
+    // Plot the points!
+    var point = chart.selectAll("circle")
+        .data(data)
+        .enter().append("circle")
+        .filter(function(d) {return d.Length > 500; }) // Subset length > 500
+        .attr("cx", function(d) { return x(d.Ref_GC); })
+        .attr("cy", function(d) { return y(d.Avg_fold); })
+        .attr("r", function(d) { return rad(d.Length); })
+        .style("fill","blue")
+        .style("fill-opacity", 0.3)
+        //.style("stroke","grey")
+        //.style("stroke-opacity",0.1)
+        // Tooltip animation
+        //   modified from: http://bl.ocks.org/d3noob/257c360b3650b9f0a52dd8257d7a2d73
+        .on("mouseover", function (d) {
+            div.transition()
+            	.duration(50)
+                .style("opacity", 0.9);
+            div.html("ID: " 
+            	+ d.ID 
+            	+ "<br /> Cov: " 
+            	+ f3r(d.Avg_fold) // Numbers reformatted (see above)
+            	+ "<br />GC: " 
+            	+ f0pc(d.Ref_GC)
+            	+ "<br /> Len: " 
+            	+ f3s(d.Length)
+            	)
+            .style("left",(d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+            })
+        .on("mouseout",function(d) {
+            div.transition()
+                .duration(90)
+                .style("opacity",0);
+            });
+    
+    // Generate summary stats
+    // Do calculations
+    var stats = [ { property: "Total length",
+    				value: d3.sum(data, function(d) { return d.Length; }),
+    				units: "bp" },
+    			  { property: "Number contigs",
+    			    value: data.map(function(d) { return d.ID; }).length,
+    				units: "" },
+    			  { property: "Max length",
+    			    value: d3.max(data, function(d) { return d.Length; }),
+    				units: "bp" },
+    			  { property: "Median length",
+    				value: d3.median(data, function (d) { return d.Length; }),
+    				units: "bp" },
+    			  { property: "Min length",
+    			    value: d3.min(data, function(d) { return d.Length; }),
+    				units: "bp" }
+    			];
+    // Output results
+    var summaryStats = d3.select(".summaryStats")
+    	.selectAll("p")
+    	.data(stats)
+    	.enter().append("p")
+    	.style("font-family","sans-serif")
+    	.style("font-size","10pt")
+    	.text(function(d) { return d.property + ": " + f3s(d.value) + d.units; });
+
+	// Generate histogram of contig lengths
+		// Produce bins
+	var lenScaleX = d3.scaleLinear()
+		.domain([0, d3.max(data, function(d) { return d.Length; })*1.2])
+		.range([0,lenplotWidth]);
+	var lenBins = d3.histogram()
+		.domain(lenScaleX.domain())
+		.thresholds(lenScaleX.ticks(20))
+		(data.map(function(d) {return d.Length; }));
+	//console.log(lenBins); // Check
+		// Set up scales
+	var lenScaleY = d3.scaleSqrt() // Prefer sqrt to log because some vals zero
+		.domain([0, d3.max(lenBins, function(d) { return d.length; })*1.1])
+		.range([lenplotHeight, 0]); // Inverse mapping of y-axis
+		// Draw x axis
+    var lenplotxAxis = d3.axisBottom(lenScaleX)
+        .ticks(10,".1s");
+    lenplot.append("g")
+        .attr("class","x axis")
+        .attr("transform","translate(0," + lenplotHeight + ")")
+        .call(lenplotxAxis);
+        // Draw y axis
+    var lenplotyAxis = d3.axisLeft(lenScaleY)
+    	.ticks(10,".1s");
+    lenplot.append("g")
+    	.attr("class","y axis")
+    	.call(lenplotyAxis);
+    	// Draw histogram elements
+    	// Adapted from: http://bl.ocks.org/mbostock/3048450
+    var bar = lenplot.selectAll(".bar")
+    	.data(lenBins)
+    	.enter().append("g")
+    	.attr("class","bar")
+    	.attr("transform", function(d) { 
+    		return "translate(" 
+    			+ lenScaleX(d.x0) 
+    			+ "," 
+    			+ lenScaleY(d.length) 
+    			+ ")"; 
+    			}
+    		);
+    bar.append("rect")
+    	.attr("x",1) // What does this do??
+    	.attr("width", lenScaleX(lenBins[0].x1) - lenScaleX(lenBins[0].x0) - 1)
+    	.attr("height", function(d) { return lenplotHeight - lenScaleY(d.length); });
+
+}
+
+function filter(lenmin) {
+    chart.selectAll("circle")
+    	.filter(function(d) { return d.Length < lenmin; })
+    	.attr("display", "none");
+    chart.selectAll("circle")
+    	.filter(function(d) { return d.Length >= lenmin; })
+    	.attr("display", "initial");
+};
